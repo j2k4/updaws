428a0448589407e76453d86ecc702772e8c0f267
Jonathan Keith <jonkeith@gmail.com>
2023-02-05 23:56:24 -0600

    Initial commit

---
+++ src/README.md
@@ -0,0 +1,22 @@
+## updaws
+
+This simple package is made for updating the AWS CLI credentials in the `~/.aws/config` file on macOS by reading from stdin. 
+
+`updaws` expects that the contents of the clipboard be in the following format:
+
+```
+[SSO_Profile_Name]
+aws_access_key_id=ASIA6GGZZ5SSAMPSES
+aws_secret_access_key=78LR3IUSAMPLE9vnw+tYLCOOVjhwKYc2pBpV
+aws_session_token=IQoJb3JpZ2luX2SAMPLEkHwdKmrgA
+```
+
+If `updaws` does not find the `aws_access_key_id`, `aws_secret_access_key`, or `aws_session_token` in your clipboard, it will not update your `~/.aws/config` file.
+
+`updaws` is for people who have multiple AWS SSO profiles and want to easily update their credentials in the `~/.aws/config` file. Maybe `aws sso login` doesn't work for you, or you just want to update your credentials without having to open the AWS CLI. You probably see a screen something like this one, and Option 1 doesn't work for you, either.
+
+![AWS SSO Login Screen](https://raw.githubusercontent.com/bradleyfalzon/updaws/master/sso-login.png)
+
+`updaws` is for Option 2 people. Click on option 2, and you'll see a screen like this one. Copy the contents of the screen, and run `updaws` in your terminal. It will update your `~/.aws/config` file with the new credentials.
+
+Go Team Option 2!
---
+++ src/app.js
@@ -0,0 +1,138 @@
+const exec = require("child_process").exec;
+const fs = require("fs");
+const readline = require("readline");
+
+// if nothing in the existing config file, use these defaults
+let _region = "us-east-2";
+let _output = "text";
+const configPath = `${process.env.HOME}/.aws/config`;
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    _output: process.stdout
+});
+
+const validateCredentials = (credentials) => {
+    const lines = credentials.split("\n");
+
+    // ignore the first line, usually something line "[SSO_Profile_Name]"
+    // we don't care about the profile name
+    lines.shift()
+
+    if (lines.length < 3) {
+        console.error("Error: Invalid credentials structure.");
+        return false;
+    }
+
+    const accessKeyLine = lines[0];
+    const secretKeyLine = lines[1];
+    const sessionTokenLine = lines[2];
+
+    if (!accessKeyLine.startsWith("aws_access_key_id=") ||
+        !secretKeyLine.startsWith("aws_secret_access_key=") ||
+        !sessionTokenLine.startsWith("aws_session_token=")) {
+        console.error("Error: Invalid credentials structure found in clipboard.");
+        return false;
+    }
+
+    console.log("Valid credentials structure found in clipboard.");
+    return true;
+};
+
+const readExistingConfig = () => {
+    try {
+        config = fs.readFileSync(configPath, "utf-8");
+    } catch (err) {
+        console.error(`Error reading config file: ${err}`);
+        return;
+    }
+
+    const lines = config.split("\n");
+    let inDefaultSection = false;
+    var region, output;
+
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+
+        if (line === "[default]") {
+            inDefaultSection = true;
+        } else if (line.startsWith("[") && line.endsWith("]")) {
+            inDefaultSection = false;
+        } else if (inDefaultSection) {
+            if (line.startsWith("_region")) {
+                region = line.split("=")[1].trim();
+            } else if (line.startsWith("_output")) {
+                output = line.split("=")[1].trim();
+            }
+        }
+    }
+
+    _region = region ? region : _region;
+    _output = output ? output : _output;
+}
+
+const updateAWSConfig = (credentials) => {
+    const lines = credentials.split('\n');
+    lines.shift(); // ignore the first line
+    const accessKeyLine = lines[0];
+    const secretKeyLine = lines[1];
+    const sessionTokenLine = lines[2];
+  
+    let config = '';
+    let foundDefaultSection = false;
+    let inDefaultSection = false;
+  
+    try {
+      const existingConfig = fs.readFileSync(configPath, 'utf-8');
+      const existingLines = existingConfig.split('\n');
+      existingLines.forEach((line) => {
+        if (line.startsWith('[default]')) {
+          foundDefaultSection = true;
+          inDefaultSection = true;
+          config += `[default]\nregion = ${_region}\noutput = ${_output}\n${accessKeyLine}\n${secretKeyLine}\n${sessionTokenLine}\n\n`;
+        } 
+        else if (inDefaultSection) {
+            if (line.startsWith('[')) {
+                inDefaultSection = false;
+                config += line + '\n';
+            }
+        }
+        else {
+          config += line + '\n';
+        }
+      });
+    } catch (error) {
+      console.error(`Error reading existing AWS config: ${error}`);
+      return;
+    }
+  
+    if (!foundDefaultSection) {
+      config += `[default]\nregion = ${_region}\noutput = ${_output}\n${accessKeyLine}\n${secretKeyLine}\n${sessionTokenLine}\n`;
+    }
+  
+    try {
+      fs.writeFileSync(configPath, config, 'utf-8');
+      console.log('AWS config file successfully updated.');
+    } catch (error) {
+      console.error(`Error writing updated AWS config: ${error}`);
+    }
+  };
+
+exec("pbpaste", (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error executing pbpaste process: ${error}`);
+        return;
+    }
+
+    const credentials = stdout.toString();
+
+
+    if (!validateCredentials(credentials)) {
+        console.log('Exiting program. No files were modified.');
+        process.exit(1);
+    }
+
+    readExistingConfig();
+    // ok to update the file now, but will still ask the user to confirm
+    updateAWSConfig(credentials);;
+});
